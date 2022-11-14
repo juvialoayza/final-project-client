f03dc044c7a76159e38b0c072b226a7f5db92260
Juvixa A <juvixa.aloayza@gmail.com>
2022-11-14 18:02:45 +0100

    profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,6 +10,19 @@ function Profile() {
 
   const [details, setDetails] = useState(null)
   const [isFetching, setIsFetching] = useState(true)
+  const [firstNameInput, setFirstNameInput] = useState("")
+  const [lastNameInput, setLastNameInput] = useState("")
+  const [emailInput, setEmailInput] = useState("")
+  const [passwordInput, setPasswordInput] = useState("")
+  const [bioCreatorInput, setBioCreatorInput] = useState("")
+
+  const handleFirstNameChange = (event) => setFirstNameInput(event.target.value)
+  const handleLastNameChange = (event) => setLastNameInput(event.target.value)
+  const handleEmailChange = (event) => setEmailInput(event.target.value)
+  const handlePasswordChange = (event) => setPasswordInput(event.target.value)
+  const handleBioCreatorChange = (event) => setBioCreatorInput(event.target.value)
+
+
 
   useEffect(() => {
     getData()
@@ -46,9 +59,28 @@ function Profile() {
   return (
     <div>
       <h3>👋 Hola {details.firstName} </h3>
-      <p>firstName: {details.firstName}</p>
+      <form>
 
+      <label htmlFor="firstName">Nombre:</label>
+      <input type="text" name="firstName" value={firstNameInput} onChange={handleFirstNameChange}></input>
+      <br />
+      <label htmlFor="lastName">Apellido:</label>
+      <input type="text" name="lastName" value={lastNameInput} onChange={handleLastNameChange}></input>
+      <br />
+      <label htmlFor="email">Email:</label>
+      <input type="email" name="email" value={emailInput} onChange={handleEmailChange}></input>
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input type="text" name="password" onChange={handlePasswordChange} ></input>
+      <br />
+      <label htmlFor="bioCreator">Biography:</label>
+      <br />
+      <textarea name="bioCreator" rows="10" cols="40" onChange={handleBioCreatorChange}>Describe tu experiencia </textarea>
+      <br />
+      {/* <label htmlFor="photoExperience">Fotos:</label>
+      <input type="file" name="photoExperience" onChange={handlePasswordChange}></input> */}
 
+      </form>
       <div>
 
         <button onClick={handleDelete}>Delete</button>
