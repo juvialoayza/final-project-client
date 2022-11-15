9a7eeccad6c12d11564ac9fcfd12aa42db9d81d3
ranier <ranieremujica@gmail.com>
2022-11-15 12:04:33 +0100

    experience details completed

--- src/pages/ExperienceDetail.jsx
+++ src/pages/ExperienceDetail.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
 import axios from "axios"
-import {experienceDetailsServices} from "../services/experience.services"
+import {experienceDetailsService} from "../services/experience.services"
 
 function ExperienceDetail() {
   const [experienceInfo, setExperienceInfo] = useState(null)
   const [isFetching, setIsFetching] = useState(true)
+  // console.log(experienceInfo)
+  const {experienceId} = useParams()
 
-  const [experienceId] = useParams()
   useEffect(() => {
     getInfoExperience()
-  })
-  getInfoExperience = async () => {
+  }, [experienceId])
+
+  const getInfoExperience = async () => {
     try {
-      const response = await experienceDetailsService()
+      const response = await experienceDetailsService(experienceId)
       console.log(response)
       setExperienceInfo(response.data)
       setIsFetching(false)
@@ -30,11 +32,14 @@ function ExperienceDetail() {
   
   return (
     <div>
-        {experienceInfo.map((eachExperience) => {
-         return(
-          <p>{eachExperience.name}</p>
-         )
-        })}
+       
+         <h2>Details</h2>
+          <p>{experienceInfo.name}</p>
+          <p>{experienceInfo.description}</p>
+          <p>{experienceInfo.price}</p>
+          <p>{experienceInfo.duration}</p>
+          <p>{experienceInfo.creator[0].firstName} {experienceInfo.creator[0].lastName}</p>
+       
     </div>
   )
 }
