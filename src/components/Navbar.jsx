2315f2b8e2844beb6be873c9145479edc9e97f0d
Juvixa A <juvixa.aloayza@gmail.com>
2022-11-16 10:05:07 +0100

    home.jsx: creación de estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { AuthContext } from "../context/auth.context"
-
+import homeBtn from "../assets/home-button.png"
 function Navbar() {
 
   const { authenticatorUser, isLoggedIn, setUser, setIsLoggedIn } = useContext(AuthContext)
@@ -12,38 +12,42 @@ function Navbar() {
   }
   return (
     <div>
-      {isLoggedIn === true ? (
-        <div className='items-menu'>
-          <div className='items-menu'>
-            <NavLink to="/">Home</NavLink>
-          </div>
-          <div className='items-menu'>
-            <NavLink to="/profile">Profile</NavLink>
-          </div>
-          <div className='items-menu'>
-          <NavLink to="/experiences">Experiences</NavLink>
-          </div>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <div className='items-menu'>
-          <NavLink to="/">Home</NavLink>
-          </div>
-          <div className='items-menu'>
-          <NavLink to="/experiences">Experiences</NavLink>
-          </div>
-          <div className='items-menu'>
-          <NavLink to="/signup">Signup</NavLink>
-          </div>
-          <div className='items-menu'>
-          <NavLink to="/login">Login</NavLink>
-          </div>
-        </div>
-      )
-      }
-    </div>
-
+      <header>
+        <nav>
+          
+          {isLoggedIn === true ? (
+            <div className='items-menu'>
+              <div className='items-menu'>
+                <NavLink to="/"> Home</NavLink>
+              </div>
+              <div className='items-menu'>
+                <NavLink to="/profile">Profile</NavLink>
+              </div>
+              <div className='items-menu'>
+                <NavLink to="/experiences">Experiences</NavLink>
+              </div>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) : (
+            <div className="items-menu">
+              <div className='item-menu'>
+                <NavLink to="/">Home</NavLink>
+              </div>
+              <div className='item-menu'>
+                <NavLink to="/experiences">Experiences</NavLink>
+              </div>
+              <div className='item-menu'>
+                <NavLink to="/signup">Signup</NavLink>
+              </div>
+              <div className='item-menu'>
+                <NavLink to="/login">Login</NavLink>
+              </div>
+            </div>
+          )}
+        </nav>
+    </header >
+    </div >
+    
   )
 }
 export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import ExperienceList from "./ExperienceList"
-import { Link} from "react-router-dom"
-import imgHeader from "../assets/img-header.jpg"
+import { Link } from "react-router-dom"
+import imgHeader from "../assets/photo-grid.png"
 import imgMadrid from "../assets/madrid.jpg"
 import imgSevilla from "../assets/sevilla.jpg"
 import "../styles/home.css"
@@ -8,68 +8,63 @@ import "../styles/home.css"
 function Home() {
   return (
     <div className="cont-img-header">
-      <img src={imgHeader} alt="experience"/>
-        <div className="description-page">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <img src={imgHeader} alt="experience" width="90%" />
+      <div className="description-page">
+        <p>Looking to get the <b>best out of your holiday?</b> Let us help by getting you a great guide to experiences that you'll never forget. We will show you hidden gems and tell you stories that only a local would know, in a different way!</p>
       </div>
 
       <div className="cont-seccion">
         <div className="title-seccion">
-          <h3>Featured places
-            <span className="view-all" id="view-all-places"> see all</span>
-            </h3>
+          <h2>Featured places</h2>
         </div>
         <div className="items-seccion">
           <div className="item-seccion">
             <div className="info-item-seccion"></div>
-            <img src={imgMadrid} alt="madrid-city" width="250px" className="img-item-seccion"/>
+            <img src={imgMadrid} alt="madrid-city" width="250px" className="img-item-seccion" />
             <div className="name-city-item-seccion">Madrid</div>
           </div>
           <div className="item-seccion">
             <div className="info-item-seccion"></div>
-            <img src={imgMadrid} alt="madrid-city" width="250px" className="img-item-seccion"/>
-            <div className="name-city-item-seccion">Madrid</div>
+            <img src={imgMadrid} alt="madrid-city" width="250px" className="img-item-seccion" />
+            <div className="name-city-item-seccion">Sevilla</div>
           </div>
           <div className="item-seccion">
             <div className="info-item-seccion"></div>
-            <img src={imgMadrid} alt="madrid-city" width="250px" className="img-item-seccion"/>
-            <div className="name-city-item-seccion">Madrid</div>
+            <img src={imgMadrid} alt="madrid-city" width="250px" className="img-item-seccion" />
+            <div className="name-city-item-seccion">Barcelona</div>
           </div>
           <div className="item-seccion">
             <div className="info-item-seccion"></div>
-            <img src={imgMadrid} alt="madrid-city" width="250px" className="img-item-seccion"/>
-            <div className="name-city-item-seccion">Madrid</div>
+            <img src={imgMadrid} alt="madrid-city" width="250px" className="img-item-seccion" />
+            <div className="name-city-item-seccion">Valencia</div>
           </div>
-          
+
         </div>
-      <h3>Experiences</h3>
-      <ExperienceList/>
+        <h2>Experiences</h2>
+        <ExperienceList />
       </div>
       <div>
-        <h3>Categories</h3>
-      <div>
-      <Link to ="">Art and Culture</Link>
+        <h2>Categories</h2>
+        <div>
+          <Link to="">Art and Culture</Link>
+        </div>
+        <div>
+          <Link to="">Food</Link>
+        </div>
+        <div>
+          <Link to="">Photography</Link>
+        </div>
+        <div>
+          <Link to="">Adventure</Link>
+        </div>
+        <div>
+          <Link to="">Entertainment</Link>
+        </div>
+        <div>
+          <Link to="">Dark Tourist</Link>
+        </div>
       </div>
-      <div>
-      <Link to ="">Food</Link>
-      </div>
-      <div>
-      <Link to ="">Photography</Link>
-      </div>
-      <div>
-      <Link to ="">Adventure</Link>
-      </div>
-      <div>
-      <Link to ="">Entertainment</Link>
-      </div>
-      <div>
-      <Link to ="">Dark Tourist</Link>
-      </div>
-      </div>
-      </div>
+    </div>
 
   )
 }
