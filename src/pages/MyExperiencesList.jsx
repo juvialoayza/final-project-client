ab3fe5bd87a53d203b7c851b1be4f1f058eddf4c
juvialoayza <juvixa.aloayza@gmail.com>
2022-12-07 23:32:57 +0100

    aceptando cambios

--- src/pages/MyExperiencesList.jsx
+++ src/pages/MyExperiencesList.jsx
@@ -7,6 +7,7 @@ import {deleteExperienceService, updateExperienceService} from "../services/expe
 import {uploadImageService} from "../services/upload.services"
 
 
+
 function MyExperiencesList() {
   const navigate = useNavigate()
   const {categoryName} = useParams()
